aaac2ac7405af4ea902d81cf0713ce67efd3be74
Michael <michaelcollins247@gmail.com>
2017-12-07 14:56:36 -0500

    no message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/DriveUntilDistance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/DriveUntilDistance.java
@@ -1,8 +1,30 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.SystemControl.HardwareController;
+
 /**
  * Created by Michael on 11/30/2017.
  */
 
-public class DriveUntilDistance {
+@Autonomous(name="BallAuto", group="MainAuto")
+public class DriveUntilDistance extends LinearOpMode {
+
+    HardwareController hwcon;
+
+    @Override
+    public void runOpMode() {
+        hwcon = new HardwareController();
+        hwcon.initHardware(hardwareMap, telemetry);
+
+        hwcon.raiseLowerArm(true);
+
+        while(opModeIsActive()) {
+            telemetry.addData("UltraSonic Distance", hwcon.getRangeUltraSonicValue());
+            hwcon.updateSensorsAndTelmetry();
+        }
+    }
+
 }
